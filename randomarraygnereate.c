6f29132602250af6d734f2f44a7d3e93e92b3c6a
chiamzheng <127980671+chiamzheng@users.noreply.github.com>
2023-09-18 20:32:40 +0800

    Update randomarraygnereate.c
    options to choose different sample sizes

--- randomarraygnereate.c
+++ randomarraygnereate.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h> // for the random number generator
-#define SIZE 5
 
-int** generate(int x, int* lengths);
+
+int** generate(int x, int size,int* lengths);
 
 int main() {
     srand(time(NULL)); // Seed the random number generator (time(NULL) for random)
 
-    int choice = 0, x = 100, generated = 0, i = 0, ele = 0,ind=0;
+    int choice = 0, x = 100, generated = 0, gen=0,i = 0, ele = 0,ind=0,size=5;
     int** arr = NULL;
-    int* arr_lengths = (int*)calloc(SIZE, sizeof(int)); // To store the lengths of arrays
+    int* arr_lengths = NULL; // To store the lengths of arrays
 
     while (choice != 5) {
         printf("1.Generate array\n");
@@ -21,34 +21,64 @@ int main() {
 
         switch (choice) {
             case 1:
-                if (generated == 0) {
-                    arr = generate(x, arr_lengths); // Pass x and arr_lengths to the generate function
-                    generated = 1;
-                }
-                else {
-                    for (i = 0; i < SIZE; i++) {
+                printf("Choose sample size:\n");
+                printf("1.Small\n");
+                printf("2.Medium\n");
+                printf("3.Large\n");
+                scanf("%d",&gen);
+                if((gen>0)&&(gen<4)&&(generated==1)){ //check if generated before and free previous arrays
+                    for (i = 0; i < size; i++) {
                         free(arr[i]);
                     }
                     free(arr);
-                    arr=generate(x, arr_lengths);
+                    free(arr_lengths);
                 }
-                break;
+                while(gen!=979797){
+                    switch(gen){ //choose size of array
+                        case 1:
+                            size=5;
+                            gen=989898;
+                            break;
+                        case 2:
+                            size=9;
+                            gen=989898;
+                            break;
+                        case 3:
+                            size=37;
+                            gen=989898;
+                            break;
+                        case 989898:
+                            arr_lengths=(int*)calloc(size, sizeof(int));// create arr_lengths array
+                            arr = generate(x, size, arr_lengths); // Pass x, size and arr_lengths to the generate function
+                            generated = 1;
+                            gen=979797;
+                            break;
+                        case 979797:
+                            break;
+                        default:
+                            printf("Invalid choice\n");
+                            gen=979797;
+                            break;
+                    }
+                }
+
+                break; // end case 1
             case 2:
                 if (generated == 1) {
                     printf("Sizes of arrays: \n");
-                    for (i = 0; i < SIZE; i++) {
+                    for (i = 0; i < size; i++) {
                         printf("Array %d length: %d\n", i, arr_lengths[i]);
                     }
                 }
                 else {
                     printf("No array generated\n");
                 }
-                break;
+                break; // end case 2
             case 3:
                 if (generated == 1) {
-                    printf("Enter index of array (0-4)\n");
+                    printf("Enter index of array (0-%d)\n",size-1);
                     scanf("%d", &ele);
-                    if (ele < 0 || ele >= SIZE) {
+                    if (ele < 0 || ele >= size) {
                         printf("Invalid index\n");
                     }
                     else {
@@ -61,26 +91,25 @@ int main() {
                 else {
                     printf("No array generated\n");
                 }
-                break;
+                break; // end case 3
             case 4:
                 if (generated == 1){
-                    printf("Enter Array (0-4)");
+                    printf("Enter Array (0-%d)",size-1);
                     scanf("%d", &ele);
-                    if (ele < 0 || ele >= SIZE) {
+                    if (ele < 0 || ele >= size) {
                         printf("Invalid index\n");
                     }
                     else{
                         printf("Choose index (0-%d)\n",arr_lengths[ele]-1);
                         scanf("%d",&ind);
                         printf("The element at %d is %d\n",ind,arr[ele][ind]);
-
                     }
                 }
 
                 else {
                     printf("No array generated\n");
                 }
-                break;
+                break; // end case 4
             default:
                 printf("Invalid choice\n");
                 break;
@@ -89,7 +118,7 @@ int main() {
 
     // Free the allocated memory before exiting
     if (generated == 1) {
-        for (i = 0; i < SIZE; i++) {
+        for (i = 0; i < size; i++) {
             free(arr[i]);
         }
         free(arr);
@@ -100,18 +129,41 @@ int main() {
     return 0;
 }
 
-int** generate(int x, int* lengths) {
-    int i = 0, j = 0, length = 100, multiplier=10; 
+int** generate(int x, int size, int* lengths) {
+    int i = 0, j = 0, length = 1000, multiplier=10;
+    int** arr = (int**)calloc(size, sizeof(int*));
+    if(size==5){
+        //no need change length or multiplier
+    }
+    else if(size==9){
+        multiplier=2;
+    }
+    else if(size==37){
+        multiplier=1000;
+    }
 
-    int** arr = (int**)calloc(SIZE, sizeof(int*));
-    for (i = 0; i < SIZE; i++) {
-        length = length * multiplier; // increasing size for current  array
+    for (i = 0; i < size; i++) {
         lengths[i] = length; // Store the length of each array
         arr[i] = (int*)calloc(length, sizeof(int));
         for (j = 0; j < length; j++) {
             arr[i][j] = (rand() % x) + 1; //assigning random values to array
         }
         printf("Array %d generated\n", i);
+
+        switch(size){
+            case 37:
+                if((length/multiplier)==10){
+                    multiplier=length;
+                }
+                length= length+multiplier;
+                break;
+            case 9:
+                multiplier = 10/multiplier; //1k,5k,10k,50k,100k,500k,1mil,5mil,10mil
+            case 5:
+                length=length*multiplier; //1k,10k,100k,1mil,10mil
+                break;
+        }
+
     }
     printf("All Arrays generated\n");
     return arr;
