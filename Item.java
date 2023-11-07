f042fd7e4cd9410259b8da808a78d77a389a2a28
sheeshzr <131577433+sheeshzr@users.noreply.github.com>
2023-11-07 23:37:35 +0800

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,14 @@
+package proj3;
+
+public class Item {
+	
+	//Attributes
+	public int weight;
+	public int value;
+	
+	//Constructor
+	public Item (int weight, int value) {
+		this.weight = weight;
+		this.value = value;
+	}
+}
